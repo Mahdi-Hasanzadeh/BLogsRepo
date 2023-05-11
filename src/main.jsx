48d9533d9c2dc9fc160571a03febf02eebdbb588
Mahdi Hasanzadeh <Mahdi.h2619@gmail.com>
2023-05-11 05:19:54 +0430

    hashrouter added

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,16 @@ import "./index.css";
 import { store } from "./redux.js";
 import { Provider } from "react-redux";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Practice from "./Components/Practice.jsx";
 // import { store } from "./ReduxPractice.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
